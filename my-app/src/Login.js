b23ce6e5d131a6c32661f53dab9417b39b2dad79
PaulineTeoulle <pa.teoulle@gmail.com>
2020-05-06 16:21:23 +0200

    Update Login page with material-ui

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,26 +1,67 @@
 import React from "react";
+import Button from '@material-ui/core/Button';
+import styled from "@material-ui/core/styles/styled";
+
+import TextField from '@material-ui/core/TextField';
+
 
 class LoginForm extends React.Component {
+
     render() {
 
-        return <div id = 'LoginPage'>
+        const MyButton = styled(Button)({
+            background: '#3F51B5',
+            borderRadius: 3,
+            border: 0,
+            color: 'white',
+            height: 23,
+            top: 398,
+            left: 0,
+        });
 
-        <div id ='LoginBody'>
-            <div id='title'>Scolendar</div>
-            <div id='serverAMU'>Serveur AMU</div>
-            <div id="loginForm">
-                <div class='input-field text-name'>
-                <input id = 'rectangle1' type="text" onChange={this.handleChange} autoFocus={this.props.autoFocus} placeholder="Nom utilisateur *" /><br/></div>
-                <div className='input-field text-password'>
-                    <input id = 'rectangle2'type="password" onChange={this.handleChange} autoFocus={this.props.autoFocus} placeholder="Mot de passe *"/> </div>
-                <button id='connexion' onClick={this.onSubmit}><label id='connexionLabel'>Connexion</label></button>
-                <div id='forgottenPassword'>Mot de passe oublié ?</div>
+        const textField = theme => ({
+            textField: {
+                marginLeft: theme.spacing.unit,
+                marginRight: theme.spacing.unit,
+                width: 200,
+                background: '#E8E8E8',
+                top: 800,
+                left: 65,
+            },
+        });
+
+        return <div id='LoginPage'>
+
+            <div id='LoginBody'>
+                <div id='title'>Scolendar</div>
+                <div id='serverAMU'>Serveur AMU</div>
+                <div id="loginForm">
+                    <TextField variant="filled"
+                               id="standard-password-input"
+                               label="UserName *"
+                               type="text"
+                               className={textField.textField}
+                               autoComplete="current-password"
+                               margin="normal"
+                               size="small"/>
+                               <br/>
+                    <TextField variant="filled"
+                               id="standard-password-input"
+                               label="Password *"
+                               type="password"
+                               className={textField.textField}
+                               autoComplete="current-password"
+                               margin="normal"
+                               size="small"/>
+
+                    <MyButton variant="contained" onClick={this.onSubmit}>Connexion</MyButton>
+                    <div id='forgottenPassword'>Mot de passe oublié ?</div>
+                </div>
+
+                <div id='footer'>En se connectant, vous acceptez les termes et conditions.
+                    <br/> © Scolendar 2020 - Tous droits réservés
+                </div>
             </div>
-            <div id='footer'>En se connectant, vous acceptez les termes et conditions.
-                <br/> © Scolendar 2020 - Tous droits réservés</div>
-
-
-        </div>
         </div>
     }
 }
