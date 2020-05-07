62b6c0e2ac21e5941b45f1e9710bd7da76fc44b7
RomeoC13 <romeo.chatel@gmail.com>
2020-05-07 13:13:31 +0200

    finishing Login Page css and stuff

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
 import styled from "@material-ui/core/styles/styled";
-
 import TextField from '@material-ui/core/TextField';
+import "./Login.css"
 
 
 class LoginForm extends React.Component {
@@ -13,20 +13,20 @@ class LoginForm extends React.Component {
             background: '#3F51B5',
             borderRadius: 3,
             border: 0,
-            color: 'white',
-            height: 23,
-            top: 398,
-            left: 0,
+            color: 'white'
         });
 
         const textField = theme => ({
             textField: {
+                position: "absolute",
                 marginLeft: theme.spacing.unit,
                 marginRight: theme.spacing.unit,
                 width: 200,
+                height : "14.746%",
                 background: '#E8E8E8',
-                top: 800,
+                top: 88888,
                 left: 65,
+
             },
         });
 
@@ -36,26 +36,30 @@ class LoginForm extends React.Component {
                 <div id='title'>Scolendar</div>
                 <div id='serverAMU'>Serveur AMU</div>
                 <div id="loginForm">
-                    <TextField variant="filled"
-                               id="standard-password-input"
-                               label="UserName *"
-                               type="text"
-                               className={textField.textField}
-                               autoComplete="current-password"
-                               margin="normal"
-                               size="small"/>
-                               <br/>
-                    <TextField variant="filled"
-                               id="standard-password-input"
-                               label="Password *"
-                               type="password"
-                               className={textField.textField}
-                               autoComplete="current-password"
-                               margin="normal"
-                               size="small"/>
-
-                    <MyButton variant="contained" onClick={this.onSubmit}>Connexion</MyButton>
+                    <div id="input-Username">
+                        <TextField variant="filled"
+                                   label="Nom d'utilisateur *"
+                                   type="text"
+                                   className={textField.textField}
+                                   autoComplete="current-password"
+                                   margin="normal"
+                                   size="small" fullWidth="true"/>
+                    </div>
+                    <br/>
+                    <div id="input-Password">
+                        <TextField variant="filled"
+                                   label="Mot de Passe *"
+                                   type="password"
+                                   className={textField.textField}
+                                   autoComplete="current-password"
+                                   margin="normal"
+                                   size="small" fullWidth="true"/>
+                    </div>
+                    <MyButton id="MyButton" variant="contained" onClick={this.onSubmit}>
+                        Connexion
+                    </MyButton>
                     <div id='forgottenPassword'>Mot de passe oublié ?</div>
+
                 </div>
 
                 <div id='footer'>En se connectant, vous acceptez les termes et conditions.
