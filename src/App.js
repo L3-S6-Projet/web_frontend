a6e9f6ad99c0d3a7a493dbf3a742efa6036d9a74
Nicolas Bourras <dev@nicolasb.fr>
2020-05-08 04:52:52 +0200

    Refactored App.css out in favour or the CssBaseLine component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import './App.css';
 import Router from './Routes/Router.js';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 class App extends Component {
 
     render() {
         return (
             <div className="App">
+                <CssBaseline />
                 <Router />
             </div>
         );
