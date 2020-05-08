1d844dab445d05a9816a15230a7d703f4b2dc74a
Nicolas Bourras <dev@nicolasb.fr>
2020-05-08 04:53:20 +0200

    Re-styled the Login route using proper sizes

--- src/Routes/Login/Login.js
+++ src/Routes/Login/Login.js
@@ -2,7 +2,6 @@
 
 import React from "react";
 import Button from '@material-ui/core/Button';
-import styled from "@material-ui/core/styles/styled";
 import TextField from '@material-ui/core/TextField';
 import "./Login.css"
 
@@ -40,7 +39,6 @@ class Login extends React.Component {
         api.login(loginRequest, (error, _data, response) => {
             this.setState({
                 loading: false,
-                ...this.state,
             });
 
             const data = JSON.parse(response.text);
@@ -69,68 +67,56 @@ class Login extends React.Component {
     }
 
     render() {
-        const MyButton = styled(Button)({
-            background: '#3F51B5',
-            borderRadius: 3,
-            border: 0,
-            color: 'white'
-        });
-
-        const textField = theme => ({
-            textField: {
-                position: "absolute",
-                marginLeft: theme.spacing.unit,
-                marginRight: theme.spacing.unit,
-                width: 200,
-                height: "14.746%",
-                background: '#E8E8E8',
-                top: 88888,
-                left: 65,
-
-            },
-        });
+        // TODO: submit on enter
+        // TODO: clear password on submit
+        // TODO: focus password field on failed submit
 
         return (
-            <div id='LoginPage'>
-                <div id='LoginBody'>
+            <div id='login-page'>
+                <div id='login-column'>
                     <div id='title'>Scolendar</div>
-                    <div id='serverAMU'>Serveur AMU</div>
-                    <div id="loginForm">
-                        <div id="input-Username">
-                            <TextField variant="filled"
-                                label="Nom d'utilisateur *"
-                                type="text"
-                                className={textField.textField}
-                                autoComplete="current-password"
-                                margin="normal"
-                                disabled={this.state.loading}
-                                size="small" fullWidth={true}
-                                onChange={this.onUsernameChange.bind(this)} />
-                        </div>
-                        <br />
-                        <div id="input-Password">
-                            <TextField variant="filled"
-                                label="Mot de Passe *"
-                                type="password"
-                                className={textField.textField}
-                                autoComplete="current-password"
-                                disabled={this.state.loading}
-                                margin="normal"
-                                size="small" fullWidth={true}
-                                onChange={this.onPasswordChange.bind(this)} />
-                        </div>
-                        <MyButton disabled={this.state.loading} id="MyButton" variant="contained" onClick={this.onSubmit.bind(this)}>
-                            Connexion
-                        </MyButton>
-                        <div id='forgottenPassword'>Mot de passe oublié ?</div>
+                    <div id='tagline'>Serveur AMU</div>
 
+                    <div class="spacer"></div>
+
+                    <div>
+                        <TextField variant="filled"
+                            label="Nom d'utilisateur *"
+                            disabled={this.state.loading}
+                            size="small"
+                            fullWidth={true}
+                            onChange={this.onUsernameChange.bind(this)} />
+
+                        <TextField variant="filled"
+                            label="Mot de Passe *"
+                            type="password"
+                            autoComplete="current-password"
+                            disabled={this.state.loading}
+                            margin="normal"
+                            size="small"
+                            fullWidth={true}
+                            onChange={this.onPasswordChange.bind(this)} />
+
+                        <div id="controls">
+                            <Button
+                                disabled={this.state.loading}
+                                id="login-button"
+                                variant="contained"
+                                onClick={this.onSubmit.bind(this)}>
+                                Connexion
+                            </Button>
+
+                            <a id='forgotten-password' href="#">Mot de passe oublié ?</a>
+                        </div>
                     </div>
 
-                    <div id='footer'>En se connectant, vous acceptez les termes et conditions.
+                    <div class="spacer"></div>
+
+                    <div id='footer'>En se connectant, vous acceptez les <a href="#">termes et conditions</a>.
                         <br /> © Scolendar 2020 - Tous droits réservés
                     </div>
                 </div>
-            </div>
+            </div >
         );
     }
 }
