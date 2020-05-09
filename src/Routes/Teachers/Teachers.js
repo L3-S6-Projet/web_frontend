24bef7ad0a02baf23765d6887fde784d2069ecaa
RomeoC13 <romeo.chatel@gmail.com>
2020-05-09 14:13:19 +0200

    added almost functional (but not designed) checkOption on Teachers admin vue

--- src/Routes/Teachers/Teachers.js
+++ src/Routes/Teachers/Teachers.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
-import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
+import {TableContainer, Table, TableHead, TableRow, TableCell, TableBody} from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
@@ -8,13 +8,22 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import "./Teachers.css"
 
 import Scolendar from '../../scolendar/src';
-import { getUser } from '../../auth.js';
+import {getUser} from '../../auth.js';
 import Splash from "../Splash/Splash";
+import Checkbox from '@material-ui/core/Checkbox';
 
 export default class Teachers extends Component {
     constructor(props) {
         super(props);
-        this.state = { teachers: [] , loaded : false};
+        this.state = {teachers: [], loaded: false, checked: [], allChecked: false};
+
+        this.checkAll = this.checkAll.bind(this);
+        this.isChecked = this.isChecked.bind(this);
+        this.check = this.check.bind(this);
+        this.unCheck = this.unCheck.bind(this);
+        this.unCheckAll = this.unCheckAll.bind(this);
+        this.checkAll = this.checkAll.bind(this);
+        this.testIfAllChecked = this.testIfAllChecked.bind(this);
     }
 
     componentDidMount() {
@@ -41,14 +50,85 @@ export default class Teachers extends Component {
                 this.setState({
                     teachers: data.teachers,
                 })
-                this.setState({loaded : true})
+                this.setState({loaded: true})
             }
         };
         apiInstance.teachersGet(opts, callback);
     }
 
+    CallCheckAll() {
+        if (this.state.allChecked) {
+            this.unCheckAll()
+        } else {
+            this.checkAll()
+        }
+    }
+
+    checkAll() {
+        let checked = [...this.state.checked];
+        this.state.teachers.forEach((teacher) => {
+            if (!this.isChecked(teacher.id))
+                checked.push(teacher.id)
+        })
+        this.setState({checked: checked})
+        this.setState({allChecked: true})
+    }
+
+    //TODO : Fix this function
+    unCheckAll() {
+        let checked = [...this.state.checked];
+        for (const teacher of this.state.teachers) {
+            if (this.isChecked(teacher.id))
+                checked.splice(this.state.checked.indexOf(teacher.id), 1)
+        }
+        this.setState({checked: checked})
+        this.setState({allChecked: false})
+    }
+
+    isChecked(id) {
+        return this.state.checked.includes(id);
+    }
+
+    unCheck(id) {
+        if (!this.isChecked(id))
+            return;
+        let checked = [...this.state.checked];
+        checked.splice(this.state.checked.indexOf(id), 1)
+        this.setState({checked: checked})
+        this.setState({allChecked: false})
+    }
+
+    check(id) {
+        if (this.isChecked(id))
+            return;
+        let checked = [...this.state.checked];
+        checked.push(id);
+        this.setState({checked: checked})
+        setTimeout(()=>{this.setState({allChecked: this.testIfAllChecked()})},1);
+
+    }
+
+    callCheck(id) {
+        if (this.state.checked.includes(id)) {
+            this.unCheck(id)
+        } else {
+            this.check(id)
+        }
+    }
+
+    testIfAllChecked() {
+        for (const teacher of this.state.teachers) {
+            if (!this.isChecked(teacher.id)){
+                console.log(this.state.checked)
+                console.log(teacher.id)
+                return false;
+            }
+        }
+        return true;
+    }
+
     render() {
-        if(!this.state.loaded)
+        if (!this.state.loaded)
             return <Splash/>
         const rows = this.state.teachers;
         return (
@@ -69,6 +149,14 @@ export default class Teachers extends Component {
                     <Table>
                         <TableHead>
                             <TableRow>
+                                <TableCell padding="checkbox">
+                                    <Checkbox
+                                        checked={this.state.allChecked}
+                                        onChange={(event) =>
+                                            this.CallCheckAll()
+                                        }
+                                    />
+                                </TableCell>
                                 <TableCell>Prénom</TableCell>
                                 <TableCell>Nom</TableCell>
                                 <TableCell>Email</TableCell>
@@ -78,6 +166,12 @@ export default class Teachers extends Component {
                         <TableBody>
                             {rows.map((row) => (
                                 <TableRow key={row.id}>
+                                    <TableCell padding="checkbox">
+                                        <Checkbox
+                                            onChange={event => this.callCheck(row.id)}
+                                            checked={this.isChecked(row.id)}
+                                        />
+                                    </TableCell>
                                     <TableCell component="th" scope="row">
                                         {row.firstName}
                                     </TableCell>
