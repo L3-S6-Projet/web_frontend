acebe06377f389ddc7dc2da53e781a78b869d6b1
PaulineTeoulle <pa.teoulle@gmail.com>
2020-05-22 18:33:41 +0200

    Home css fixed (Thanks to Gauthier who told me how to use GRID)

--- src/Routes/StudentsView/Home.js
+++ src/Routes/StudentsView/Home.js
@@ -17,52 +17,37 @@ export class Home extends Component {
         return (
             <div className="home-page">
                 <div id='title'>Accueil</div>
-                <Paper>
                     <Grid item xs container direction="row" spacing={3}>
                         <Grid item xs={10}>
-                            <Paper>Prochain cours</Paper>
-                        </Grid>
-                        <Grid item xs={2}>
-                            <Paper>Liens</Paper>
-                        </Grid>
-
-
-                        <Grid item xs={12} container>
                             <Grid item xs container direction="row" spacing={3}>
+                                <Grid item xs={12}>
+                                    <Paper>Prochain cours</Paper>
+                                </Grid>
                                 <Grid item xs={8}>
                                     <Paper>Dernières modifs</Paper>
                                 </Grid>
                                 <Grid item xs={4}>
-                                    <Paper>
+
                                         <Grid item container direction="column" spacing={2}>
-                                            <Grid item xs={4}>
-                                                <Paper>
+
                                                     <Grid item container direction="row" spacing={1}>
                                                         <Grid item xs={12}>
                                                             <Paper>Progrès</Paper>
                                                         </Grid>
-                                                    </Grid>
-                                                </Paper>
-                                            </Grid>
 
-
-
-                                            <Grid item xs={4}>
-                                                <Paper>
-                                                    <Grid item container direction="row" spacing={1}>
                                                         <Grid item xs={12}>
                                                             <Paper>Contacts</Paper>
                                                         </Grid>
                                                     </Grid>
-                                                </Paper>
-                                            </Grid>
+
                                         </Grid>
-                                    </Paper>
                                 </Grid>
                             </Grid>
                         </Grid>
+                        <Grid item xs={2}>
+                            <Paper>Liens</Paper>
+                        </Grid>
                     </Grid>
-                </Paper>
             </div>
         );
     }
