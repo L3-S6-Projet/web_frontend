b2f8ba1f6cc35f0890be38d015d65ccf2cb87813
PaulineTeoulle <pa.teoulle@gmail.com>
2020-05-25 13:49:44 +0200

    Add Home stuff (not good but here it is)

--- src/Routes/StudentsView/Home.js
+++ src/Routes/StudentsView/Home.js
@@ -9,6 +9,7 @@ import Scolendar from '../../scolendar/src';
 import {getUser} from '../../auth.js';
 import SelectedDate from "../../Components/Calendar/SelectedDate";
 import LinearProgress from "@material-ui/core/LinearProgress";
+import ListItem from "@material-ui/core/ListItem";
 
 export class Home extends Component {
     constructor(props) {
@@ -19,12 +20,14 @@ export class Home extends Component {
             selectedDate: SelectedDate.today(),
             view: 'day',
             nextOccupancy: [],
+            subjectsStudent:[],
         }
     }
 
     componentDidMount() {
         this.loadModifications();
         this.loadNextOccupancy();
+        this.loadSubjects();
     }
 
     loadNextOccupancy() {
@@ -57,6 +60,34 @@ export class Home extends Component {
         apiInstance.studentsIdOccupanciesGet(id, opts, callback);
     }
 
+
+    loadSubjects() {
+        const defaultClient = Scolendar.ApiClient.instance;
+        const token = defaultClient.authentications['token'];
+        token.apiKey = getUser().token;
+        token.apiKeyPrefix = 'Bearer';
+
+        const apiInstance = new Scolendar.RoleStudentApi();
+
+        const user = getUser();
+        const id = user.user.id ;
+
+
+        const callback = (error, data, response) =>  {
+            if (error) {
+                console.error(error);
+            } else {
+
+                console.log('API called successfully. Returned data: ');
+                this.setState({
+                    subjectsStudent: data.subjects,
+                })
+                this.setState({loaded: true})
+            }
+        };
+        apiInstance.studentsIdSubjectsGet(id, callback);
+    }
+
     loadModifications() {
         const defaultClient = Scolendar.ApiClient.instance;
         const token = defaultClient.authentications['token'];
@@ -80,23 +111,52 @@ export class Home extends Component {
         apiInstance.profileLastOccupanciesModificationsGet(callback);
     }
 
+
+
     render() {
+        let start = new Date(this.state.nextOccupancy.start * 1000);
+        let end = new Date(this.state.nextOccupancy.end * 1000);
+        function pad(i) {
+            if (i < 10) {
+                i = "0" + i;
+            }
+            return i;
+        }
+        let startHour = pad(start.getHours()) + ':' + pad(start.getMinutes());
+        let endHour = pad(end.getHours()) + ':' + pad(end.getMinutes());
+
+
+        const lastModif = this.state.modif.map((onemodif, index) =>
+            <ListItem secondary="Nom"  key={index}>  {onemodif.modificationType} {onemodif.occupancy.subjectName} pour les {onemodif.occupancy.className} </ListItem>
+    );
+
+
+        const contacts = this.state.subjectsStudent.map((subject, index) =>
+            <ListItem  key={index}> {subject.teachers.map( (prof, index) =>
+                <ListItem   key={index}>{prof.lastName} </ListItem> )}</ListItem>
+        );
+
+        const links = this.state.subjectsStudent.map((subject, index) =>
+                <ListItem   key={index}>{subject.name} </ListItem>
+        );
+
         return (
             <div className="home-page">
                 <div id='title'>Accueil</div>
 
                 <Grid item xs container direction="row" spacing={6}>
-                    <Grid item xs={10}>
+                    <Grid item xs={9}>
                         <Grid item xs container direction="row" spacing={6}>
                             <Grid item xs={12}>
-                                <Paper>Votre prochain cours est dans TEMPS minutes
-                                    en {this.state.nextOccupancy.classroomName} de {this.state.nextOccupancy.start} à {this.state.nextOccupancy.end}.
+                                <Paper>Votre prochain cours est en {this.state.nextOccupancy.classroomName} de {startHour} à {endHour}.
                                     C&apos;est un cours
                                     de {this.state.nextOccupancy.subjectName} avec {this.state.nextOccupancy.teacherName}.
                                 </Paper>
                             </Grid>
                             <Grid item xs={8}>
-                                <Paper>Dernières modifs</Paper>
+                                <Paper> <div id='title-progress'>Dernières modifications</div>
+                                    <ul>{lastModif}</ul>
+                                </Paper>
                             </Grid>
                             <Grid item xs={4}>
 
@@ -112,15 +172,18 @@ export class Home extends Component {
                                         </Grid>
 
                                         <Grid item xs={12}>
-                                            <Paper>Contacts</Paper>
+                                            <Paper> <div id='title-progress'>Contact</div>
+                                                <ul>{contacts}</ul>
+                                            </Paper>
                                         </Grid>
                                     </Grid>
                                 </Grid>
                             </Grid>
                         </Grid>
                     </Grid>
-                    <Grid item xs={2}>
-                        <Paper>Liens</Paper>
+                    <Grid item xs={3}>
+                        <Paper> <div id='title-progress'>Liens</div>
+                            <ul>{links}</ul></Paper>
                     </Grid>
                 </Grid>
 
